5c4be69f08093a145456b5dc9e1a665a1c2cf92b
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-09-19 21:36:02 +0100

    done header section

--- vite-project/src/components/Header.tsx
+++ vite-project/src/components/Header.tsx
@@ -5,14 +5,26 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <nav>
+      <nav className={Classes["headers"]}>
         <section className={Classes["logo"]}>
           <img src={Logo} alt="logo" />
         </section>
         <ul className={Classes["listItem"]}>
           <li>
-            <Link to="/"></Link>
+            <Link to="/">Home</Link>
           </li>
+          <li>
+            <a>Overview</a>
+          </li>
+          <li>
+            <a>FAQs</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+          <div className={Classes["btn"]}>
+            <button>Register</button>
+          </div>
         </ul>
       </nav>
     </>
