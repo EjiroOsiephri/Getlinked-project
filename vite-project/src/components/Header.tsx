e00e94f50a8cc4b6f08c867c2c183594e6d070bd
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-09-19 21:48:20 +0100

    done header section

--- vite-project/src/components/Header.tsx
+++ vite-project/src/components/Header.tsx
@@ -1,15 +1,31 @@
 import Classes from "../sass/Header.module.scss";
 import Logo from "../assets/getlinked.png";
 import { Link } from "react-router-dom";
+import { FaTimes, FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 const Header = () => {
+  const [click, setClick] = useState(false);
+
+  function showHamburger() {
+    setClick(!click);
+  }
   return (
     <>
       <nav className={Classes["headers"]}>
         <section className={Classes["logo"]}>
           <img src={Logo} alt="logo" />
         </section>
-        <ul className={Classes["listItem"]}>
+        <ul
+          style={
+            click
+              ? {
+                  left: "0",
+                }
+              : {}
+          }
+          className={Classes["listItem"]}
+        >
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -26,6 +42,25 @@ const Header = () => {
             <button>Register</button>
           </div>
         </ul>
+        <div className={Classes["hamburger-section"]} onClick={showHamburger}>
+          {click ? (
+            <FaTimes
+              style={{
+                color: "white",
+                cursor: "pointer",
+                fontSize: "20px",
+              }}
+            />
+          ) : (
+            <FaBars
+              style={{
+                color: "white",
+                cursor: "pointer",
+                fontSize: "20px",
+              }}
+            />
+          )}
+        </div>
       </nav>
     </>
   );
