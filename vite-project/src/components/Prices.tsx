b0723bba7244eac316410ff8e722823918310b34
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-09-22 09:23:27 +0100

    Finished rewardsomponents

--- vite-project/src/components/Prices.tsx
+++ vite-project/src/components/Prices.tsx
@@ -1,20 +1,33 @@
 import rewards from "../assets/Rewards.png";
 import tropthy from "../assets/9486889 1.png";
+import Classes from "../sass/Prices.module.scss";
+import star from "../assets/star.png";
+import star2 from "../assets/sata gra.png";
 
 const Prices = () => {
   return (
     <>
-      <main>
-        <section>
+      <main className={Classes["main-prizes"]}>
+        <section className={Classes["trophy-container"]}>
           <img src={tropthy} alt="trophy" />
         </section>
-        <section>
+        <section className={Classes["rewards-container"]}>
+          <img src={star2} alt="" />
           <h1>
             Prizes and <span>Rewards</span>
           </h1>
+          <p>Highlights of the prizes and rewards for the participants</p>
           <img src={rewards} alt="" />
         </section>
       </main>
+      <img
+        style={{
+          marginLeft: "3rem",
+        }}
+        src={star}
+        alt=""
+      />
+      <div className="line"></div>
     </>
   );
 };
--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import SecondHero from "../components/SecondHero";
 import Faq from "../components/Faqs";
 import Timeline from "../components/Timeline";
+import Prices from "../components/Prices";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <SecondHero />
       <Faq />
       <Timeline />
+      <Prices />
     </>
   );
 };
