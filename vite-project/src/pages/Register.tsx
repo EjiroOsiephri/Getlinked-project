97990166a19c8946ca3356fb590341c4b31dfc14
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-09-22 22:27:09 +0100

    Gotten data

--- vite-project/src/pages/Register.tsx
+++ vite-project/src/pages/Register.tsx
@@ -4,8 +4,26 @@ import starGra from "../assets/sata gra.png";
 import threeD from "../assets/3d-graphic-designer-showing-thumbs-up-png 1.png";
 import star from "../assets/star.png";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Register = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function getCategoryList() {
+      try {
+        const res = await axios.get(
+          "https://backend.getlinked.ai/hackathon/categories-list"
+        );
+
+        setData(res.data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    }
+    getCategoryList();
+  }, []);
+
   return (
     <>
       <Header />
@@ -36,9 +54,17 @@ const Register = () => {
               <input placeholder="What is your group project topic" />
             </div>
             <div className={Classes["message"]}>
-              <label htmlFor="message">Project topic</label>
+              <label htmlFor="message">Category</label>
               <select id="dropdown" name="fruit">
-                <option value="apple">Apple</option>
+                {data?.map((item: any) => {
+                  return <option>{item.name}</option>;
+                })}
+              </select>
+            </div>
+            <div className={Classes["message"]}>
+              <label htmlFor="message">Group size</label>
+              <select id="dropdown" name="fruit">
+                <option value="apple"></option>
                 <option value="banana">Banana</option>
                 <option value="cherry">Cherry</option>
                 <option value="grape">Grape</option>
